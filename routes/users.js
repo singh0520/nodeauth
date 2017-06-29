afded872282fb7b9840e70b8d766332e1d583ec0
Nikhil Singh <singh0520@gmail.com>
2017-06-29 21:23:45 +0530

    temp

--- routes/users.js
+++ routes/users.js
@@ -8,8 +8,34 @@ router.get('/', function(req, res, next) {
 
 router.get('/register', function(req, res, next) {
   res.render('register', {
-    'title': 'Register'
+    'title': 'register'
   });
 });
+
+router.get('/login', function(req, res, next) {
+  res.render('login', {
+    'title': 'login'
+  });
+});
+router.post('/users/register' function(req, res){
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+});
+
+if(req.file.profileimage){
+  console.log('uploading file...');
+
+  var profileo = req.files.profileimage.originalname;
+  var profileimagen = req.files. profileimage.name;
+  var profileimagemime = req.files. profileimage.mimetype;
+  var profileimagepath = req.files. profileimage.path;
+  var profileimagesize = req.files. profileimage.size;
+  var profileimageExt = req.files. profileimage.extension;
+} else{
+  var profileimagen = "noimage.png"
+}
 
 module.exports = router;
